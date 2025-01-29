015f501f6259638293ed64ed423a67505458244e
camilo cardona <106347705+camilo-cardona-automas@users.noreply.github.com>
2025-01-28 20:42:01 -0500

    allow hosts

--- project_config/settings.py
+++ project_config/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1']
 
 # Application definition
 
